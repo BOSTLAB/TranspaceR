c529f39af89e9e170e4aafe7d89c8a3866b3e22f
BOSTLAB <bost.lab.curie@gmail.com>
2025-04-15 16:37:28 +0200

    Update Save_comparison_plots.R

--- R/Save_comparison_plots.R
+++ R/Save_comparison_plots.R
@@ -13,10 +13,9 @@
 #'
 #' @return  NULL This function does not return a value; it saves PNG files.
 #' @export
-#' @import spatstat
 #' 
 Save_comparison_plots = function(Clustering,Annotation_cells_renamed,Umap_result,Output_path,Method,Tissue,scaling_factor=2) {
-  ppp_temp = ppp(x = Meta_data$cell_centroid_x,y = Meta_data$cell_centroid_y,
+  ppp_temp = spatstat::ppp(x = Meta_data$cell_centroid_x,y = Meta_data$cell_centroid_y,
                  window = owin(xrange = range(Meta_data$cell_centroid_x),yrange = range(Meta_data$cell_centroid_y)))
   ratio <- table(Clustering, Annotation_cells_renamed)
   filtered_table <- as.data.frame(ratio)
