c15bac423b0bb861bc2bafbe2ab2de18635b820a
BOSTLAB <bost.lab.curie@gmail.com>
2025-12-09 11:02:42 +0100

    Update package

--- R/QC_Gene_threshold.R
+++ R/QC_Gene_threshold.R
@@ -9,7 +9,8 @@
 #' @param Tissue A character string indicating the type of tissue being analyzed.
 #' @param Output_path A character string specifying the directory where the output PDF will be saved.
 #' @return A list containing the Otsu threshold and the separability score.
-#' @importFrom Matrix colSums, rowSums
+#' @importFrom Matrix colSums
+#' @importFrom Matrix rowSums
 #' @export
 
 QC_Gene_threshold = function(Expression_file,Meta_data,Method,Tissue,Output_path) {
